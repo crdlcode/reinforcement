f50b1bd9abfef63a5c1d3ed8cb218e116f2dac12
curtischatch <curtis.chat.hk@gmail.com>
2018-07-17 17:29:48 -0400

    if statements added

--- exercise1.rb
+++ exercise1.rb
@@ -1,6 +1,7 @@
 documentary_rec = "Minimalism"
 drama_rec = "Gone Girl"
 comedies_rec = "The Hangover"
+dramadey_rec = "The Secret Life of Walter Mitty"
 
 puts "Do you like documentaries?"
 doc_ans = gets.chomp.downcase
@@ -11,4 +12,13 @@ doc_ans
 puts "do you like comedy movies"
 com_ans = gets.chomp.downcase
 
-if doc
+if doc_ans == "yes"
+  puts "I will recommend #{documentary_rec}"
+end
+if doc_and == "no"
+  if (dram_ans == "yes") && (com_ans == "yes")
+    puts "You like comedy and drama, I recommend #{dramadey_rec}"
+  elsif (dram_ans == "yes")
+    puts "I recommend #{drama_rec}"
+  elsif (com_ans == "yes")
+    puts "I recommend #{comedies_rec}"
