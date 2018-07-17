8aaab53641479d2b98dff860638ca63ab6418fdc
curtischatch <curtis.chat.hk@gmail.com>
2018-07-17 17:16:39 -0400

    added qs

---
+++ exercise1.rb
@@ -0,0 +1,14 @@
+documentary_rec = "Minimalism"
+drama_rec = "Gone Girl"
+comedies_rec = "The Hangover"
+
+puts "Do you like documentaries?"
+doc_ans = gets.chomp.downcase
+
+puts "do you like drama movies"
+doc_ans
+
+puts "do you like comedy movies"
+com_ans = gets.chomp.downcase
+
+if doc
